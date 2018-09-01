65dd422d56c110319d978b488d95d9f96feb159f
Lee Savarese <lee.s@nc.rr.com>
2018-09-01 11:28:23 -0400

    added responsiveness and color contrast

--- js/main.js
+++ js/main.js
@@ -175,10 +175,13 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
+  
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
+  
+  
 
   return li
 }
