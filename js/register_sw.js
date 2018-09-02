f2118b485c2dfa0bbb7260d9dd08f0a28d8d2c1f
Lee Savarese <lee.s@nc.rr.com>
2018-09-02 16:13:58 -0400

    fixed scope error

--- js/register_sw.js
+++ js/register_sw.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker
 			//.register('../sw.js', {scope: "/"})
-			.register('../sw.js', {scope: "https://leesav3.github.io/restaurant/reviews"})
+			.register('../sw.js', {scope: "https://leesav3.github.io/restaurant-reviews/"})
 			.then(reg => console.log('Service Worker Registered'))
 			.catch(error => console.log('Service Worker NOT Registered- Error: ' + error))
 	});
